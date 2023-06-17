42d5a653764966bd7b2e507fd57d6fda8e0e7452
rohanrdy <89927943+rohanrdy@users.noreply.github.com>
2023-06-17 17:12:36 +0530

    remove extra week2 code

--- camel/camel.py
+++
@@ -1,9 +0,0 @@
-text = input("camelCase: ")
-output = ""
-
-for char in text:
-    if char.isupper():
-        output += '_' + char.lower()
-    else:
-        output += char
-print("snake_case:", output)
