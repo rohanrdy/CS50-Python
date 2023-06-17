f364b8d19663794265572ce10166d0f9f3bca853
rohanrdy <89927943+rohanrdy@users.noreply.github.com>
2023-06-17 16:41:26 +0530

    Update plates.py

--- week2/plates/plates.py
+++ week2/plates/plates.py
@@ -5,9 +5,23 @@ def main():
     else:
         print("Invalid")
 
-
 def is_valid(s):
-    if s[0:2].isalpha() and len(s)<7 and s[-1].isnumeric():
+    if s[0:2].isalpha() and 1<len(s)<7 and s.isalnum() and no_middle_num(s):
         return True
+
+def no_middle_num(s):
+    temp = 0
+    for i in range(3): # checking from the last to see if any number comes before an alphabet
+        if s[(len(s)-1)-i].isalpha():
+            if s[(len(s)-i-1)-1].isnumeric():
+                temp+=1
+    if temp > 0: # means that numbers came in the middle of the plate
+        return False
+    if temp == 0: # to check if the first number is a zero
+        for j in range(len(s)-1):
+            if s[j].isalpha():
+                if s[j+1] == "0":
+                    return False
+    return True
 
 main()
