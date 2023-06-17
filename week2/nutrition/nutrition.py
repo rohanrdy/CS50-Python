677b151f8e5e5aaff3d72af0dcceddc41ec8f56d
rohanrdy <89927943+rohanrdy@users.noreply.github.com>
2023-06-17 17:08:15 +0530

    nutrition.py code

---
+++ week2/nutrition/nutrition.py
@@ -0,0 +1,27 @@
+fruit = input("Input: ").lower()
+
+list = {
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 60,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80
+}
+
+if fruit in list:
+    print(f"Calories: {list[fruit]}")
