e3c6477dcab736687b9d62733e6520d092bb7b77
rohanrdy <89927943+rohanrdy@users.noreply.github.com>
2023-06-28 23:57:10 +0530

    pset6 q1 code

---
+++ week6/lines/lines.py
@@ -0,0 +1,32 @@
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) == 2:
+        file_path = sys.argv[1]
+    else:
+        sys.exit("Too many command-line arguments")
+
+    if file_path[-3:] == ".py":
+        try:
+            file = open(file_path).read().splitlines()
+        except FileNotFoundError:
+            sys.exit("File does not exist")
+        else:
+            print(get_lines(file))
+    else:
+        sys.exit("Not a Python file")
+
+
+def get_lines(file):
+    count = 0
+    for i in file:
+        if i != "" and i.strip()[0] != "#": # strip used to account for "Assume that any line that starts with #, optionally preceded by whitespace, is a comment"
+            count += 1
+    return count
+
+
+if __name__ == "__main__":
+    main()
